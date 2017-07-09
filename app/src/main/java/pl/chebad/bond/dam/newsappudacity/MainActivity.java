7a0a357dac4eba0de140625311f7502cf9039dcd
Damian <chebad@interia.pl>
2017-07-09 16:12:51 +0200

    Initial commit

---
+++ app/src/main/java/pl/chebad/bond/dam/newsappudacity/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.chebad.bond.dam.newsappudacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
